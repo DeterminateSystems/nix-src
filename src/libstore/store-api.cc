f0143f56d000882e0e346d4b99bac32c9e51e7a2
Eelco Dolstra <edolstra@gmail.com>
2026-03-25 19:16:53 +0100

    When doing concurrent substitutions of the same path, download only once
    Previously, `LocalStore::addToStore()` always read `info.narSize`
    bytes from its source. This caused `copyStorePath()` (used by
    substitution) to always initiate a download even when the store path
    is valid after the path lock is acquired. So if N clients trigger a
    substition of some large store path, then that store path would be
    downloaded N times.
    
    Now `LocalStore::addToStore()` no longer reads from its source if the
    path turns out to be valid. In that case, `srcStore.narFromPath()` in
    `copyStorePath()` doesn't get called (since coroutines are lazy), so
    no download will be started.
    
    If `addToStore()`'s new behaviour is a problem for the caller
    (e.g. when the NAR is part of a larger stream), then it's the caller's
    responsibility now to skip `info.narSize` bytes. This can be done
    using the `EnsureRead` source wrapper class.

--- src/libstore/store-api.cc
+++ src/libstore/store-api.cc
@@ -206,7 +206,8 @@ void Store::addMultipleToStore(Source & source, RepairFlag repair, CheckSigsFlag
                 .version = 16,
             });
         info.ultimate = false;
-        addToStore(info, source, repair, checkSigs);
+        EnsureRead wrapper{source, info.narSize};
+        addToStore(info, wrapper, repair, checkSigs);
     }
 }
 
@@ -920,6 +921,9 @@ std::shared_ptr<const ValidPathInfo> copyStorePath(
     info->ultimate = false;
 
     info->provenance = addCopiedProvenance(info->provenance, srcStore);
+
+    if (getEnv("_NIX_TEST_CONCURRENT_SUBSTITUTION"))
+        std::this_thread::sleep_for(std::chrono::seconds(1));
 
     auto source = sinkToSource(
         [&](Sink & sink) {
