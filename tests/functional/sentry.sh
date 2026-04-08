953049c85fbc4ab294d93e23b4fbe38d6f19f9d5
Eelco Dolstra <edolstra@gmail.com>
2026-04-08 15:08:45 +0200

    Add sentry test

---
+++ tests/functional/sentry.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+source common.sh
+
+unset NIX_DISABLE_SENTRY
+
+ulimit -c 0
+
+sentryDir="$TEST_HOME/.cache/nix/sentry"
+
+nix --version
+if ! [[ -d $sentryDir ]]; then
+    skip "not built with sentry support"
+fi
+
+for type in segfault assert logic-error; do
+    rm -rf "$sentryDir"
+
+    (! nix crash "$type")
+
+    [[ -e $sentryDir/last_crash ]]
+
+    envelopes=("$sentryDir"/*.run/*.envelope)
+    if [[ ! -e "${envelopes[0]}" ]]; then
+        fail "No crash dump found in $sentryDir after crash"
+    fi
+done
