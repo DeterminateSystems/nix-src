952269caa7eb46017b5fe5ece0fe2132385916af
Eelco Dolstra <edolstra@gmail.com>
2026-05-05 16:25:17 +0200

    Formatting

--- src/nix/baked-flake.nix
+++ src/nix/baked-flake.nix
@@ -1,13 +1,13 @@
 {
-  outputs = { self }: 
+  outputs =
+    { self }:
     let
       data = builtins.fromJSON (builtins.readFile ./outputs.json);
 
-      convert = output:
+      convert =
+        output:
         if output ? children then
-          builtins.mapAttrs (childName: child:
-            convert child
-          ) output.children
+          builtins.mapAttrs (childName: child: convert child) output.children
         else if output ? "derivation" then
           {
             type = "derivation";
@@ -16,17 +16,13 @@
             meta.description = output.shortDescription;
             drvPath = builtins.fakeDerivation {
               name = output.derivation.name;
-              outputs = builtins.mapAttrs (outputName: output:
-                { path = output; }
-              ) output.derivation.outputs;
-            } ;
+              outputs = builtins.mapAttrs (outputName: output: { path = output; }) output.derivation.outputs;
+            };
             outPath = output.derivation.outputs.out; # FIXME
             outputName = "out"; # FIXME
           }
         else
           throw "Output not supported in a baked flake.";
     in
-      builtins.mapAttrs (outputName: output:
-        convert output.output
-      ) data;
+    builtins.mapAttrs (outputName: output: convert output.output) data;
 }
