deb3de4616ea6911d56ef738033a40d1c0f3723d
Eelco Dolstra <edolstra@gmail.com>
2026-08-20 15:58:46 +0200

    Remove fallback behaviour for RemoteStore::addTempRoots()
    The fallback makes `nix copy` very slow when copying to a older daemon
    that doesn't support the AddTempRoots worker protocol operation. Of
    course, this is less correct, but it's the behaviour we've had for
    years prior to 7beda55f66a4cef7780a46a93ca975313fcbbc74.
    
    This reverts commit 334f160af34bdae2fa9439a3c2ddfd44a93ab9f6.

--- src/libstore/build/derivation-goal.cc
+++ src/libstore/build/derivation-goal.cc
@@ -62,12 +62,9 @@ Goal::Co DerivationGoal::haveDerivation(bool storeDerivation)
     if (!drv->type().hasKnownOutputPaths())
         experimentalFeatureSettings.require(Xp::CaDerivations);
 
-    StorePathSet outputPaths;
     for (auto & i : drv->outputsAndOptPaths(worker.store))
         if (i.second.second)
-            outputPaths.insert(*i.second.second);
-
-    worker.store.addTempRoots(outputPaths);
+            worker.store.addTempRoot(*i.second.second);
 
     /* We don't yet have any safe way to cache an impure derivation at
        this step. */
